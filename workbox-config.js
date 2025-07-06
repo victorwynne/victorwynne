bd0847abf57793d8a7ec48dc5e4dbb0c9767a6e2
Victor Wynne <81656443+victorwynne@users.noreply.github.com>
2025-07-06 10:06:31 -0400

    Added service worker config

---
+++ workbox-config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  globDirectory: '_site/',
+  globPatterns: [
+    '**/*.{html,css,js,png,jpg,jpeg,gif,svg,ico,xml,json}'
+  ],
+  swDest: '_site/sw.js',
+  ignoreURLParametersMatching: [
+    /^utm_/,
+  ],
+  runtimeCaching: [{
+    urlPattern: /\.(?:png|jpg|jpeg|svg|gif)$/,
+    handler: 'StaleWhileRevalidate',
+    options: {
+      cacheName: 'images-cache',
+      expiration: {
+        maxEntries: 50,
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      },
+    },
+  },
+  {
+    urlPattern: /\.(?:js|css)$/,
+    handler: 'StaleWhileRevalidate',
+    options: {
+      cacheName: 'static-resources-cache',
+      expiration: {
+        maxEntries: 50,
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+      },
+    },
+  },
+  {
+    urlPattern: ({ url }) => url.origin === self.location.origin,
+    handler: 'NetworkFirst',
+    options: {
+      cacheName: 'html-pages-cache',
+    },
+  }]
+};
