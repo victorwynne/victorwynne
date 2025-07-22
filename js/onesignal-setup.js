d9b86393fdf39aada5f6b6d9a6ee5ce735dec45f
Victor Wynne <81656443+victorwynne@users.noreply.github.com>
2025-07-22 18:33:30 -0400

    default.html onesignal-setup.js

--- js/onesignal-setup.js
+++
@@ -1,6 +0,0 @@
-window.OneSignalDeferred = window.OneSignalDeferred || [];
-OneSignalDeferred.push(async function(OneSignal) {
-  await OneSignal.init({
-    appId: "1860e89f-f790-4e30-af88-ee26a306c7ea"
-  });
-});
